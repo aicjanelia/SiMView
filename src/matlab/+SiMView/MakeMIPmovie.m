da30f676b89123e8161402c90032137690611c7f
ericwait <eric@waitphoto.com>
2019-02-26 14:10:00 -0500

    Made a new MIP movie maker

---
+++ src/matlab/+SiMView/MakeMIPmovie.m
@@ -0,0 +1,78 @@
+function MakeMIPmovie(rootDir,subDirectory,overwrite,separateColors)
+
+    if (~exist('overwrite','var') || isempty(overwrite))
+        overwrite = false;
+    end
+    if (~exist('separateColors','var') || isempty(separateColors))
+        separateColors = false;
+    end
+
+    [imMeta,structured] = SiMView.GetMetadata(fullfile(rootDir,subDirectory));
+
+    colorsStd = [0,1,0;1,0,1;0,1,1;1,0,0;1,1,0;0,0,1];
+    
+    prefix = imMeta.DatasetName;
+    frameDir = fullfile(rootDir,subDirectory,'movieFrames');
+    if (separateColors)
+        prefix = [prefix,'_sepColors'];
+        frameDir = [frameDir,'_sepColors'];
+    end
+
+    if (~exist(frameDir,'dir'))
+        mkdir(frameDir);
+    elseif (overwrite)
+        rmdir(frameDir,'s');
+        pause(5)
+        mkdir(frameDir);
+    else
+        return
+    end
+
+    parfor t=1:imMeta.NumberOfFrames
+        intensityImage = SiMView.GetImages(imMeta,structured,t);
+        if(separateColors)
+            curIm = intensityImage(:,:,:,1,:,1);
+            for cm = 2:size(intensityImage,6)
+                curIm = cat(4,curIm,intensityImage(:,:,:,1,:,cm));
+            end
+        else
+            curIm = SiMView.CombineChannelPairs(intensityImage);
+            curIm = SiMView.CombineCameras(curIm);
+        end
+
+        if (size(curIm,4)==1)
+            colors = [1,1,1];
+        else
+            colors = colorsStd(1:size(curIm,4),:);
+        end
+
+        imOrtho = ImUtils.MakeOrthoSliceProjections(curIm,colors,imMeta.PixelPhysicalSize(1),imMeta.PixelPhysicalSize(3));
+
+        sz = size(imOrtho);
+        if (sz(1)>sz(2))
+            imOrtho = imrotate(imOrtho,90);
+        end
+        imOrtho = imresize(imOrtho,[1080,NaN]);
+        imOrtho = ImUtils.MakeImageXYDimEven(imOrtho);
+
+        imwrite(imOrtho,fullfile(frameDir,sprintf('%s_%04d.tif',prefix,t)));
+    end
+
+
+    if (imMeta.NumberOfFrames<10)
+        return
+    end
+
+    fps = min(60,max(imMeta.NumberOfFrames)/10);
+    fps = max(fps,7);
+    fps = round(fps);
+
+    MovieUtils.MakeMP4_ffmpeg(1,imMeta.NumberOfFrames,frameDir,fps,[prefix,'_']);
+
+    movieDir = fullfile(rootDir,'MIPmovies');
+    if (~exist(movieDir,'dir'))
+        mkdir(movieDir);
+    end
+
+    copyfile(fullfile(frameDir,[prefix,'_','.mp4']),movieDir);
+end
