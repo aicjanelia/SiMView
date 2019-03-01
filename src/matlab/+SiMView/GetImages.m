d3d56a2ce6f3752663055cf371478473b304d67a
ericwait <eric@waitphoto.com>
2019-03-01 08:17:41 -0500

    Get image data from unstructured directories of tiffs

--- src/matlab/+SiMView/GetImages.m
+++ src/matlab/+SiMView/GetImages.m
@@ -27,43 +27,56 @@ function im = GetImages(imMetadata,structured,frames,chans,cameras)
         [frameList,dList] = Utils.GetNumsFromFiles(imMetadata.imageDir,'TM(\d+)',ext);
         chanList = Utils.GetNumsFromFiles(imMetadata.imageDir,'CHN(\d+)',ext);
         camList = Utils.GetNumsFromFiles(imMetadata.imageDir,'CM(\d+)',ext);
+        plnList = Utils.GetNumsFromFiles(imMetadata.imageDir,'PLN(\d+)',ext);
+        
+        if (isempty(plnList))
+            plnList = false(size(frameList));
+        end
         
         frameMask = frameList==frames(1)-1;
         chanMask = chanList==chans(1)-1;
         camMask = camList==cameras(1)-1;
+        plnMask = plnList==0;
         
-        filePath = fullfile(imMetadata.imageDir,dList(frameMask & chanMask & camMask).name);
+        filePath = fullfile(imMetadata.imageDir,dList(frameMask & chanMask & camMask & plnMask).name);
         
         switch ext
             case 'stack'
                 im = MicroscopeData.ReaderRawStack(imMetadata.Dimensions,filePath,imMetadata.PixelFormat);
             case 'tif'
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                % TODO
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                im = imread(filePath);
             otherwise
                 error('Unknown file type');
         end
         
-        im = zeros([size(im),length(chans),length(frames),length(cameras)],'like',im);
+        im = zeros([size(im,1),size(im,2),imMetadata.Dimensions(3),length(chans),length(frames),length(cameras)],'like',im);
         
         for cm=1:length(cameras)
+            camMask = camList==cameras(cm)-1;
             for t=1:length(frames)
+                frameMask = frameList==frames(t)-1;
                 for c=1:length(chans)
-                    
-                    frameMask = frameList==frames(t)-1;
                     chanMask = chanList==chans(c)-1;
-                    camMask = camList==cameras(cm)-1;
-                    
-                    filePath = fullfile(imMetadata.imageDir,dList(frameMask & chanMask & camMask).name);
                     
                     switch ext
                         case 'stack'
+                            fMask = frameMask & chanMask & camMask;
+                            if (~any(fMask))
+                                continue
+                            end
+                            filePath = fullfile(imMetadata.imageDir,dList(fMask).name);
                             im(:,:,:,c,t,cm) = MicroscopeData.ReaderRawStack(imMetadata.Dimensions,filePath,imMetadata.PixelFormat);
                         case 'tif'
-                            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                            % TODO
-                            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                            for z=1:imMetadata.Dimensions(3)
+                                plnMask = plnList==z-1;
+                                
+                                fMask = frameMask & chanMask & camMask & plnMask;
+                                if (~any(fMask))
+                                    continue
+                                end
+                                filePath = fullfile(imMetadata.imageDir,dList(fMask).name);
+                                im(:,:,z,c,t,cm) = imread(filePath);
+                            end
                         otherwise
                             error('Unknown file type');
                     end
@@ -72,8 +85,6 @@ function im = GetImages(imMetadata,structured,frames,chans,cameras)
         end
         
     else
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % TODO
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        error('TODO deal with sturctured directories');
     end
 end
