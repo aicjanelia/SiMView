0b73b16229c028e10efd81dc495d23465905ff56
ericwait <eric@waitphoto.com>
2019-03-05 10:44:26 -0500

    Improved checking for movies already made

--- src/matlab/+SiMView/MakeMIPmovie.m
+++ src/matlab/+SiMView/MakeMIPmovie.m
@@ -31,60 +31,19 @@ function MakeMIPmovie(rootDir,subDirectory,overwrite,separateColors,fps,maxSec)
         prefix = [prefix,'_sepColors'];
         frameDir = [frameDir,'_sepColors'];
     end
-
-    if (~exist(frameDir,'dir'))
-        mkdir(frameDir);
-        writeFiles = true;
-    elseif (overwrite)
-        rmdir(frameDir,'s');
-        pause(5)
-        mkdir(frameDir);
-        writeFiles = true;
-    elseif (dynamicFPS)
-        return
-    end
     
-    tic
-    fprintf(1,'Making movie %s...',prefix);
-    if (writeFiles)   
-        for t=1:imMeta.NumberOfFrames
-%         parfor t=1:imMeta.NumberOfFrames
-            intensityImage = SiMView.GetImages(imMeta,structured,t);
-            if(separateColors)
-                curIm = intensityImage(:,:,:,1,:,1);
-                for cm = 2:size(intensityImage,6)
-                    curIm = cat(4,curIm,intensityImage(:,:,:,1,:,cm));
-                end
-            else
-                curIm = SiMView.CombineChannelPairs(intensityImage);
-                curIm = SiMView.CombineCameras(curIm);
-            end
-            
-            if (size(curIm,4)==1)
-                colors = [1,1,1];
-            else
-                colors = colorsStd(1:size(curIm,4),:);
-            end
-            
-            imOrtho = ImUtils.MakeOrthoSliceProjections(curIm,colors,imMeta.PixelPhysicalSize(1),imMeta.PixelPhysicalSize(3));
-            
-            sz = size(imOrtho);
-            if (sz(1)>sz(2))
-                imOrtho = imrotate(imOrtho,90);
-            end
-            imOrtho = imresize(imOrtho,[1080,NaN]);
-            imOrtho = ImUtils.MakeImageXYDimEven(imOrtho);
-            
-            imwrite(imOrtho,fullfile(frameDir,sprintf('%s_%04d.tif',prefix,t)));
-        end
-        
-        if (imMeta.NumberOfFrames<10)
-            return
-        end
-        
+    if (dynamicFPS)
         fps = min(60,max(imMeta.NumberOfFrames)/10);
         fps = max(fps,7);
         fps = round(fps);
+    end
+    prefixFPS = sprintf('%s_%dfps_',prefix,fps);
+    
+    dateDir = regexp(subDirectory, filesep, 'split');
+    dateDir = dateDir{1};
+    movieDir = fullfile(rootDir,'MIPmovies',dateDir);
+    if (~exist(movieDir,'dir'))
+        mkdir(movieDir);
     end
     
     maxFrame = imMeta.NumberOfFrames;
@@ -94,19 +53,76 @@ function MakeMIPmovie(rootDir,subDirectory,overwrite,separateColors,fps,maxSec)
     end
     
     if (isempty(maxFrame) || maxFrame<20)
-        fprintf('Skipping\n');
         return
+    elseif (~exist(frameDir,'dir'))
+        mkdir(frameDir);
+        writeFiles = true;
+    elseif (overwrite)
+        rmdir(frameDir,'s');
+        pause(5)
+        mkdir(frameDir);
+        writeFiles = true;
+    elseif (exist(fullfile(movieDir,[prefixFPS,'.mp4']),'file'))
+        return
+    else
+        d = dir(frameDir);
+        mask = [d.isdir];
+        d = d(~mask);
+        if (isempty(d))
+            writeFiles = true;
+        elseif (dynamicFPS)
+            return
+        end
+    end
+    
+    tic
+    fprintf(1,'Making movie %s...',prefix);
+    if (writeFiles)   
+%         for t=1:imMeta.NumberOfFrames
+        parfor t=1:imMeta.NumberOfFrames
+            try
+                intensityImage = SiMView.GetImages(imMeta,structured,t);
+                if(separateColors)
+                    curIm = intensityImage(:,:,:,1,:,1);
+                    for cm = 2:size(intensityImage,6)
+                        curIm = cat(4,curIm,intensityImage(:,:,:,1,:,cm));
+                    end
+                else
+                    curIm = SiMView.CombineChannelPairs(intensityImage);
+                    curIm = SiMView.CombineCameras(curIm);
+                end
+
+                if (size(curIm,4)==1)
+                    colors = [1,1,1];
+                else
+                    colors = colorsStd(1:size(curIm,4),:);
+                end
+
+                imOrtho = ImUtils.MakeOrthoSliceProjections(curIm,colors,imMeta.PixelPhysicalSize(1),imMeta.PixelPhysicalSize(3));
+
+                sz = size(imOrtho);
+                if (sz(1)>sz(2))
+                    imOrtho = imrotate(imOrtho,90);
+                end
+                imOrtho = imresize(imOrtho,[1080,NaN]);
+                imOrtho = ImUtils.MakeImageXYDimEven(imOrtho);
+            catch err
+                warning(err.message)
+                fprintf('\n');
+                continue
+            end
+            
+            imwrite(imOrtho,fullfile(frameDir,sprintf('%s_%04d.tif',prefix,t)));
+        end
+        
+        if (imMeta.NumberOfFrames<10)
+            return
+        end
     end
 
     MovieUtils.MakeMP4_ffmpeg(1,maxFrame,frameDir,fps,[prefix,'_']);
     
-    prefixFPS = sprintf('%s_%dfps_',prefix,fps);
     movefile(fullfile(frameDir,[prefix,'_','.mp4']),fullfile(frameDir,[prefixFPS,'.mp4']));
-
-    movieDir = fullfile(rootDir,'MIPmovies');
-    if (~exist(movieDir,'dir'))
-        mkdir(movieDir);
-    end
 
     copyfile(fullfile(frameDir,[prefixFPS,'.mp4']),movieDir);
     fprintf(1,'took %s\n',Utils.PrintTime(toc,imMeta.NumberOfFrames));
