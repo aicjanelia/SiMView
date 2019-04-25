68292631f309a2931fd9286e244e6abe111276b2
Eric Wait <eric@waitphoto.com>
2019-04-25 16:28:04 -0400

    Moved getting images (structured and unstructured) to their own functions

---
+++ src/matlab/+SiMView/GetImagesStructured.m
@@ -0,0 +1,51 @@
+function im = GetImagesStructured(imMetadata,frames,chans,cameras,verbose)
+    %Assuming uint16... This might be bad.
+    im = zeros([imMetadata.Dimensions([2,1,3]),length(chans),length(frames),length(cameras)],'uint16');
+    ext = 'tif'; %deal with non-tif data
+    
+    i = 1;
+    for t=1:length(frames)
+        fileList = dir(fullfile(imMetadata.imageDir,'SPM00',sprintf('TM%05d',frames(t)),'ANG000',['*.',ext]));
+        
+        dNames = {fileList.name}';
+        chanList = Utils.GetNumFromStr(dNames,'CHN(\d+)');
+        camList = Utils.GetNumFromStr(dNames,'CM(\d+)');
+        plnList = Utils.GetNumFromStr(dNames,'PLN(\d+)');
+        
+        if (isempty(plnList))
+            plnList = ones(size(chanList));
+        end
+        
+        for cm=1:length(cameras)
+            camMask = camList==cameras(cm)-1;
+            for c=1:length(chans)
+                chanMask = chanList==chans(c)-1;
+                for z=1:max(plnList)
+                    plnMask = z-1==plnList;
+                    
+                    fMask = camMask & chanMask & plnMask;
+                    if (~any(fMask))
+                        continue
+                    end
+                    
+                    if (strcmpi(ext,'tif'))
+                        curIm = imread(fullfile(fileList(fMask).folder,fileList(fMask).name));
+                        if (cm==2)
+                            curIm = curIm(:,end:-1:1,:,:);
+                        end
+                        im(:,:,z,c,t,cm) = curIm;
+                    else
+                        %TODO
+                    end
+                end
+            end
+            if (verbose)
+                prgs.PrintProgress(i);
+            end
+            i = i+1;
+        end
+    end
+    if (verbose)
+        prgs.ClearProgress(true)
+    end
+end
