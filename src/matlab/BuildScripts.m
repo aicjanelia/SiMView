68995f437ae81e0941018e169a1c08886dc6310a
David Ackerman <ackermand@janelia.hhmi.org>
2019-06-14 16:22:55 -0400

    *** (SAS)SQUASH WITH JVM COMMIT

--- src/matlab/BuildScripts.m
+++ src/matlab/BuildScripts.m
@@ -3,5 +3,5 @@ binDirectory = fullfile(fileparts(fileparts(currentDirectory)), 'bin');
 if (~exist(binDirectory,'dir'))
     mkdir(binDirectory);
 end
-eval(sprintf('mcc -m -R -nojvm -v ClusterReadAndWriteRestructuredKLBs.m -d %s', binDirectory));
-eval(sprintf('mcc -m -R -nojvm -v ClusterFuseViews.m -d %s', binDirectory));
+eval(sprintf('mcc -m -R -nodesktop -v ClusterReadAndWriteRestructuredKLBs.m -d %s', binDirectory));
+eval(sprintf('mcc -m -R -nodesktop -v ClusterFuseViews.m -d %s', binDirectory));
