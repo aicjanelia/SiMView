5d5bde2194b2dd5d2722e2faabb13b3a5c892549
David Ackerman <ackermand@janelia.hhmi.org>
2019-06-17 12:33:58 -0400

    Remove last frame during restructure if it is empty

--- src/matlab/+SiMView/ReadAndWriteRestructuredKLBs.m
+++ src/matlab/+SiMView/ReadAndWriteRestructuredKLBs.m
@@ -1,14 +1,20 @@
 function ReadAndWriteRestructuredKLBs(imMetadata, currentImMetadata, lightsheetIndex, cameraIndex, scopeChannels, structured, outputDir, frame)
-scopeChannelsForCurrentView = scopeChannels(lightsheetIndex:imMetadata.NumberOfChannels:end);
-currentImageData = SiMView.GetImages(imMetadata, structured, frame, scopeChannelsForCurrentView, cameraIndex);
-if cameraIndex == 2
-    currentImageData = flip(currentImageData,2); %Flipped because of how camera 2 acquires wrt camera 1
-end
-% Write KLB
-MicroscopeData.WriterKLB(currentImageData, 'path', outputDir, 'imageData', currentImMetadata, 'datasetName', currentImMetadata.DatasetName,'timeRange',[frame, frame],'filePerT',true,'filePerC',true,'writeJson', false);
-orthoSliceProjections = ImUtils.MakeOrthoSliceProjections(currentImageData, currentImMetadata.ChannelColors, currentImMetadata.PixelPhysicalSize(1), currentImMetadata.PixelPhysicalSize(3));
-if (~exist(fullfile(outputDir, 'MovieFrames'),'dir'))
-    mkdir(fullfile(outputDir, 'MovieFrames'));
-end
-imwrite(orthoSliceProjections, fullfile(outputDir, 'MovieFrames', sprintf('%04d.tif',frame)));
+    scopeChannelsForCurrentView = scopeChannels(lightsheetIndex:imMetadata.NumberOfChannels:end);
+    currentImageData = SiMView.GetImages(imMetadata, structured, frame, scopeChannelsForCurrentView, cameraIndex);
+    if frame == currentImMetadata.NumberOfFrames && nnz(currentImageData) == 0
+        % If last frame is empty
+        currentImMetadata.NumberOfFrames = currentImMetadata.NumberOfFrames - 1;
+        MicroscopeData.CreateMetadata(outputDir,currentImMetadata);
+    else
+        if cameraIndex == 2
+            currentImageData = flip(currentImageData,2); %Flipped because of how camera 2 acquires wrt camera 1
+        end
+        % Write KLB
+        MicroscopeData.WriterKLB(currentImageData, 'path', outputDir, 'imageData', currentImMetadata, 'datasetName', currentImMetadata.DatasetName,'timeRange',[frame, frame],'filePerT',true,'filePerC',true,'writeJson', false);
+        orthoSliceProjections = ImUtils.MakeOrthoSliceProjections(currentImageData, currentImMetadata.ChannelColors, currentImMetadata.PixelPhysicalSize(1), currentImMetadata.PixelPhysicalSize(3));
+        if (~exist(fullfile(outputDir, 'MovieFrames'),'dir'))
+            mkdir(fullfile(outputDir, 'MovieFrames'));
+        end
+        imwrite(orthoSliceProjections, fullfile(outputDir, 'MovieFrames', sprintf('%04d.tif',frame)));
+    end
 end
