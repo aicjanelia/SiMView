19b6e16234f68a3db69b92a7db775f47acb739c4
Eric Wait <eric@waitphoto.com>
2019-09-04 11:34:18 -0400

    Using str2double instead of older str2num

--- src/matlab/ClusterFuseViews.m
+++ src/matlab/ClusterFuseViews.m
@@ -1,5 +1,6 @@
 function ClusterFuseViews(rootDir, transform, channel, frame)
-channel = str2num(channel);
-frame = str2num(frame);
-transform = jsondecode(transform);
-SiMView.FuseViews(rootDir, transform, channel, frame);
+    channel = str2double(channel);
+    frame = str2double(frame);
+    transform = jsondecode(transform);
+    SiMView.FuseViews(rootDir, transform, channel, frame);
+end
