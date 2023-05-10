b76bd9861d0b2b0e54cfe0e1cee8654dfb0d04b2
Rachel Lee <65371136+ScientistRachel@users.noreply.github.com>
2023-05-10 14:02:09 -0400

    Add option to flip camera numbering

--- src/python/renameSiMView4BigStitcher.py
+++ src/python/renameSiMView4BigStitcher.py
@@ -14,7 +14,8 @@ Parser for command line arguments
 def parse_args():
     parser = argparse.ArgumentParser(description='Converts a SiMView data directory into a folder of symlinks for BigStitcher dataset definition')
     parser.add_argument('input', type=Path, help='path to image files')
-    parser.add_argument('-removeSlices', type=Path, help='number of z-slices to remove. Defaults to zero.', dest='removeSlices', default=0)
+    parser.add_argument('-flipZ', action='store_true', help='flag to change the numerical order of the z-slices on CM1. Defaults to false.', dest='flipZ', default=False)
+    parser.add_argument('-flipCamera', action='store_true', help='flag to change the numerical order of the cameras. Defaults to false.', dest='flipCamera', default=False)
     args = parser.parse_args()
 
     if not args.input.is_dir():
@@ -22,12 +23,10 @@ def parse_args():
 
     return args
 
- 
-
 """
-Parser for filenames + metadata
+Use the first timepoint to determine the range of the various file types
 """
-def parse_files(path1,Nremove):
+def parse_filenames(path1):
 
     # First find all the timepoint folders
     tFolders = os.listdir(path1)
@@ -56,11 +55,20 @@ def parse_files(path1,Nremove):
             if m.group(2) not in chList:
                 chList.append(m.group(2))
             if m.group(3) not in zList:
-                zList.append(m.group(3))
+                zList.append(m.group(3)) 
+
+    return tList,chList,camList,zList
+
+"""
+Reorder the z-slices of CM1 so that 0 is renamed to the maxZ and maxZ is 0.
+"""
+def run_zFlip(path1):
+
+    tList,chList,camList,zList = parse_filenames(path1)
     zMax = int(max(zList))
     # print(zMax)
 
-    # rename the image files
+    # flip the order of the z-slices
     for idx, t in enumerate(tList):
         tNow = 'TM'+str(tList[idx])
         tfNow = path1 / tNow / 'ANG000'
@@ -113,19 +121,110 @@ def parse_files(path1,Nremove):
                 # print(cmd)
 
 
+
+"""
+Reorder the cameras such that CM1->CM0 and CM0->CM1.
+"""
+def run_cameraFlip(path1):
+
+    tList,chList,camList,zList = parse_filenames(path1)
+    if len(camList)>2:
+        exit(f'ERROR: Expected maximum two cameras.')
+    elif len(camList)==2:
+        if not((camList[0]=='0') & (camList[1]=='1')):
+            exit(f'ERROR: Expected the two cameras to be CM0 and CM1.')
+    elif len(camList)==1:
+        if not(camList[0]=='0' | camList[0]=='1'):
+            exit(f'ERROR: Expected the cameras to be either CM0 or CM1.')
+    else:
+        exit(f'ERROR: Unexpected camera naming error.')
+
+
+    # flip the cameras 0-->1, 1-->0
+    for idx, t in enumerate(tList):
+        tNow = 'TM'+str(tList[idx])
+        tfNow = path1 / tNow / 'ANG000'
+        print(tNow)
+
+        # Rename twice because otherwise you run issue that, e.g., z0300 already exists when you want to rename z0000 --> z0300
+        # If the file names aren't the exact number of zero-padding, etc., the BigStitcher plugin can't read it
+        # So --> rename to a tmp name, then switch back to the same format as before.
+
+        # tmp renaming
+        files = os.listdir(tfNow) # ASSUMPTION: one angle experiment set up
+        patternF = re.compile('SPC00_TM'+tList[idx]+'_ANG000_CM(\d)_CHN(\d{2})_PH0_PLN(\d{4}).tif')
+        for f in files:
+            m = patternF.fullmatch(f)
+            if m:
+                newCam = 1-int(m.group(1))
+                newFile = 'SPC00_'+tNow+'_ANG000_CM'+str(newCam).zfill(3)+'_CHN'+m.group(2)+'_PH0_PLN'+m.group(3)+'.tif'
+
+                if os.name == 'nt': # windows
+                    cmd = 'ren '+ str(tfNow/f) + ' ' + newFile                        
+                else:
+                    cmd = 'mv '+ str(tfNow/f) + ' ' + str(tfNow/newFile)
+                threading.Thread(target=os.system,args=(cmd,)).start()
+                # os.system(cmd)
+                # print(cmd)
+
+        # Don't proceed to the next step until these threads finish running
+        # This avoids duplicate issues
+        Nrunning = threading.active_count()
+        # print(Nrunning)
+        while Nrunning>1:
+            sleep(0.05)
+            Nrunning = threading.active_count()
+            # print(Nrunning)
+
+        # final renaming
+        files = os.listdir(tfNow) # ASSUMPTION: one angle experiment set up
+        patternF = re.compile('SPC00_TM'+tList[idx]+'_ANG000_CM(\d{3})_CHN(\d{2})_PH0_PLN(\d{4}).tif')
+        for f in files:
+            m = patternF.fullmatch(f)
+            if m:
+                newCam = int(m.group(1))
+                # print(newCam)
+                newFile = 'SPC00_'+tNow+'_ANG000_CM'+str(newCam).zfill(0)+'_CHN'+m.group(2)+'_PH0_PLN'+m.group(3)+'.tif'
+
+                if os.name == 'nt': # windows
+                    cmd = 'ren '+ str(tfNow/f) + ' ' + newFile                        
+                else:
+                    cmd = 'mv '+ str(tfNow/f) + ' ' + str(tfNow/newFile)
+                threading.Thread(target=os.system,args=(cmd,)).start()
+                # os.system(cmd)
+                # print(cmd)
+
+
 if __name__ == '__main__':
     # get command line arguments
     args = parse_args()
 
-    # make the symlinks
-    data = parse_files(args.input,args.removeSlices)
+    if args.flipZ & args.flipCamera:
+        exit(f'ERROR: This script does not yet support flipping Z and the camera simultaneously. \nPlease run each command separately to control the order of operations. \nzFlip only flips CM1.')    
+    elif args.flipZ: # If requested, flip Z
+        run_zFlip(args.input)
 
-    # make sure none of the threads are still running before reporting that you are done
-    Nrunning = threading.active_count()
-    # print(Nrunning)
-    while Nrunning>1:
-        sleep(0.05)
+        # make sure none of the threads are still running before reporting that you are done
         Nrunning = threading.active_count()
         # print(Nrunning)
+        while Nrunning>1:
+            sleep(0.05)
+            Nrunning = threading.active_count()
+            # print(Nrunning)
 
-    print('Done')
+        print('Done with zFlip')
+    elif args.flipCamera:
+        run_cameraFlip(args.input)
+
+        # make sure none of the threads are still running before reporting that you are done
+        Nrunning = threading.active_count()
+        # print(Nrunning)
+        while Nrunning>1:
+            sleep(0.05)
+            Nrunning = threading.active_count()
+            # print(Nrunning)
+
+        print('Done with cameraFlip')
+    else:
+        print('No changes in file names were requested and no files were renamed.')
+        print('Use -flipZ or -flipCamera to identify the type of renaming required.')
